8cd7bde5c7a066c1099d109963d89450d8c86be7
glitvjak <gatislitvjaks@gmail.com>
2023-12-06 15:18:51 +0000

    worked on likes and dislikes for comments

--- pkg/handlers/utils.go
+++ pkg/handlers/utils.go
@@ -281,3 +281,141 @@ func HandleSubmitComment(w http.ResponseWriter, r *http.Request) {
 	// Redirect back to the main page or the post's page
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
+
+// HandleLikeComment handles the liking of a comment.
+func HandleLikeComment(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Check if the user is logged in
+	session, err := auth.Store.Get(r, "user-session")
+	if err != nil || session.Values["user_id"] == nil {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	userID := session.Values["user_id"].(int) // Assuming user_id is stored as an int
+
+	commentID, err := strconv.Atoi(r.FormValue("comment_id"))
+	if err != nil {
+		http.Error(w, "Invalid comment ID", http.StatusBadRequest)
+		return
+	}
+
+	// Check if the user has previously liked the comment
+	hasLiked, err := db.HasUserLikedComment(userID, commentID)
+	if err != nil {
+		http.Error(w, "Failed to check previous like status", http.StatusInternalServerError)
+		return
+	}
+
+	// Remove the like if the user had previously liked the comment
+	if hasLiked {
+		_, err = db.RemoveCommentLike(userID, commentID)
+		if err != nil {
+			http.Error(w, "Failed to remove the previous like", http.StatusInternalServerError)
+			return
+		}
+
+		// Redirect back to the main page or the comment's page
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	// Check if the user has previously disliked the comment
+	hasDisliked, err := db.HasUserDislikedComment(userID, commentID)
+	if err != nil {
+		http.Error(w, "Failed to check previous dislike status", http.StatusInternalServerError)
+		return
+	}
+
+	// Remove the dislike if the user had previously disliked the comment
+	if hasDisliked {
+		_, err = db.RemoveCommentDislike(userID, commentID)
+		if err != nil {
+			http.Error(w, "Failed to remove the previous dislike", http.StatusInternalServerError)
+			return
+		}
+	}
+
+	// Add logic to handle liking the comment (e.g., update the database for like)
+	_, err = db.AddCommentLike(userID, commentID)
+	if err != nil {
+		http.Error(w, "Failed to like the comment", http.StatusInternalServerError)
+		return
+	}
+
+	// Redirect back to the main page or the comment's page
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+// HandleDislikeComment handles the disliking of a comment.
+func HandleDislikeComment(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Check if the user is logged in
+	session, err := auth.Store.Get(r, "user-session")
+	if err != nil || session.Values["user_id"] == nil {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	userID := session.Values["user_id"].(int) // Assuming user_id is stored as an int
+
+	commentID, err := strconv.Atoi(r.FormValue("comment_id"))
+	if err != nil {
+		http.Error(w, "Invalid comment ID", http.StatusBadRequest)
+		return
+	}
+
+	// Check if the user has previously disliked the comment
+	hasDisliked, err := db.HasUserDislikedComment(userID, commentID)
+	if err != nil {
+		http.Error(w, "Failed to check previous dislike status", http.StatusInternalServerError)
+		return
+	}
+
+	// Remove the dislike if the user had previously disliked the comment
+	if hasDisliked {
+		_, err = db.RemoveCommentDislike(userID, commentID)
+		if err != nil {
+			http.Error(w, "Failed to remove the previous dislike", http.StatusInternalServerError)
+			return
+		}
+
+		// Redirect back to the main page or the comment's page
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	// Check if the user has previously liked the comment
+	hasLiked, err := db.HasUserLikedComment(userID, commentID)
+	if err != nil {
+		http.Error(w, "Failed to check previous like status", http.StatusInternalServerError)
+		return
+	}
+
+	// Remove the like if the user had previously liked the comment
+	if hasLiked {
+		_, err = db.RemoveCommentLike(userID, commentID)
+		if err != nil {
+			http.Error(w, "Failed to remove the previous like", http.StatusInternalServerError)
+			return
+		}
+	}
+
+	// Add logic to handle disliking the comment (e.g., update the database for dislike)
+	_, err = db.AddCommentDislike(userID, commentID)
+	if err != nil {
+		http.Error(w, "Failed to dislike the comment", http.StatusInternalServerError)
+		return
+	}
+
+	// Redirect back to the main page or the comment's page
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
